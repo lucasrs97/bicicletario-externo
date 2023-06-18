7e605cbfb6055cee4cf96e5f8ef6f5eedcc9f6be
lucasramalho.s97 <lucasramalho.s97@gmail.com>
2023-06-18 01:30:30 -0300

    Implementação da classe EmailService

---
+++ src/main/java/com/api/bicicletario/service/EmailService.java
@@ -0,0 +1,21 @@
+package com.api.bicicletario.service;
+
+import com.api.bicicletario.exception.ValidatorException;
+
+public class EmailService {
+
+    public void enviarEmail(String email, String mensagem) {
+        try {
+            System.out.println("E-mail enviado com sucesso. " + "\n"
+                    + "Destinatário: " + email + '\n'
+                    + "Mensagem: " + mensagem + '\n');
+        } catch (Exception e) {
+            throw new ValidatorException("Não foi possível enviar o e-mail");
+        }
+    }
+
+    public boolean emailValido(String email) {
+        return true;
+    }
+
+}
